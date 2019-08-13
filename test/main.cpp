d17b6fe29af5992ef602304962032431f1a454c3
Gene Koval <genya@archvm.local>
2019-08-13 19:53:08 +0300

    Created separate exec function for test

--- test/main.cpp
+++ test/main.cpp
@@ -8,10 +8,11 @@ using std::cout;
 using std::endl;
 using std::runtime_error;
 
-int main() {
-    launcher program(PROJECT, VERSION);
+int exec() {
+    cout << "Running test program." << endl;
+    throw runtime_error("A test error.");
+}
 
-    return program.start([]() {
-        throw runtime_error("A test error.");
-    });
+int main() {
+    return launcher(PROJECT, VERSION).start(exec);
 }
